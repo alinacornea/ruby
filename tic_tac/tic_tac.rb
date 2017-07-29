6cd84086879eefd42fdad09c997b4aa4f1533f27
Alina CORNEA <alcornea@e1z2r9p2.42.us.org>
2017-07-29 02:52:25 -0700

    finish

--- tic_tac/tic_tac.rb
+++ tic_tac/tic_tac.rb
@@ -32,23 +32,19 @@ class Game
 
   def initialize(name1, name2)
     $board = Board.new
-    @player1 = Player.new(name1, 'X')
-    @player2 = Player.new(name2, 'O')
-  end
-
-  def game_over?
-    $board.check_step ? true : false
+    @player1 = Player.new(name1, 'X'.red)
+    @player2 = Player.new(name2, 'O'.green)
   end
 
   def play
     $board.intructions
-    @player1.turn = true
+    player1.turn = true
     put_mark
   end
 
   def put_mark
-    until game_over?
-      current_player = @player1.turn ? @player1 : @player2
+    until $board.check_step?(player1.name, player2.name)
+      current_player = player1.turn ? player1 : player2
       puts "#{current_player.name}, mark!".bold
       current_player.selection
       switch_players
@@ -69,19 +65,24 @@ end
 
 class Board
 
-  WINS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], #Horizontal
-     [0, 3, 6], [1, 4, 7], [2, 5, 8], #Vertical
-     [0, 4, 8], [2, 4, 6]]            #Diagonal
+  WINS =
+  [[0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8]]
 
   attr_accessor :matrix, :win
 
   def initialize
       @matrix = Array.new(3) {Array.new(3) }
-      @winner_board = Array.new(3) {Array.new(8)}
       @win = false
   end
 
-  def display()
+  def display
     puts
     puts " #{matrix[0][0]} | #{matrix[0][1]} | #{matrix[0][2]}".bold
     puts "---|---|---".bold
@@ -92,46 +93,47 @@ class Board
   end
 
   def intructions
-    @matrix[0][0] = 1
-    @matrix[0][1] = 2
-    @matrix[0][2] = 3
-    @matrix[1][0] = 4
-    @matrix[1][1] = 5
-    @matrix[1][2] = 6
-    @matrix[2][0] = 7
-    @matrix[2][1] = 8
-    @matrix[2][2] = 9
+    matrix[0][0] = 1
+    matrix[0][1] = 2
+    matrix[0][2] = 3
+    matrix[1][0] = 4
+    matrix[1][1] = 5
+    matrix[1][2] = 6
+    matrix[2][0] = 7
+    matrix[2][1] = 8
+    matrix[2][2] = 9
     puts "To mark a cell just type the number of the cell 1-9: ".bold
     display
   end
 
 
   def parse_selection(sel, symbol)
-      row = @matrix.detect{|a| a.include?(sel)}
-      x = @matrix.index(row)
-      y = row.index(sel)
-      @matrix[x][y] = symbol
-
+      row = matrix.detect{|a| a.include?(sel)}
+      x = matrix.index(row)
+      if x.is_a? Integer
+        y = row.index(sel)
+        matrix[x][y] = symbol
+      else
+        puts "This place is taken.".bold
+      end
     display
   end
 
-  def check_step
-    WINS.each do |win|
-      print @matrix
-      if check_combination?('X', win)
-        return win
-      elsif check_combination?('O', win)
-        return win
-      else
-        return false
-      end
-    end
-  end
+  def check_step?(player1, player2)
+  combo = 0
+  arr = matrix.flatten
+  while combo < WINS.length
+    current_combo = WINS[combo]
 
-  def check_combination?(player, win)
-    win.all? do |position|
-      @matrix[position] == player
+    p1 = current_combo.all? { |position| arr[position] == "\e[0;31;49mX\e[0m" }
+    p2 = current_combo.all? { |position| arr[position] == "\e[0;32;49mO\e[0m" }
+    if p1 == true || p2  == true
+      p1 ? (puts "#{player1}, is the winner!".yellow.bold) : (puts "#{player2}, is the winner!").yellow.bold
+      return current_combo
     end
+    combo += 1
+  end
+  false
   end
 
 end
@@ -139,7 +141,7 @@ end
 
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Welcome to TIC_TAC_TOE game!".bold.cyan
+  puts "Welcome to TIC_TAC_TOE game!\n".bold.cyan
   puts "Type the name of first player: ".bold.yellow
   name1 = gets.chomp
   puts "Type the name of second player: ".bold.yellow
